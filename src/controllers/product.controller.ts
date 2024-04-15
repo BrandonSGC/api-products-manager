62f44bab45dff2204e92383967ece84924139b20
BrandonSGC <brangmz20@gmail.com>
2024-04-14 19:54:43 -0600

    Get Products by id and Update Product

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -5,8 +5,7 @@ import colors from "colors";
 export const getProducts = async (req: Request, res: Response) => {
   try {
     const products = await Product.findAll();
-    res.status(200).json({products});
-    
+    res.status(200).json({ products });
   } catch (error) {
     res
       .status(500)
@@ -16,14 +15,30 @@ export const getProducts = async (req: Request, res: Response) => {
       error
     );
   }
-}
+};
+
+export const getProductsById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const product = await Product.findByPk(id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json({ product });
+  } catch (error) {
+    console.error(
+      colors.red("An error has occurred while getting the product")
+    );
+  }
+};
 
 export const createProduct = async (req: Request, res: Response) => {
   try {
     const product = await Product.create(req.body);
-    res.status(201).json({ message: "Product Created...", product: product });
-    console.log(colors.cyan("Product created successfully..."));
-
+    res.status(201).json({ message: "Product created successfully...", product: product });
   } catch (error) {
     res
       .status(500)
@@ -31,6 +46,28 @@ export const createProduct = async (req: Request, res: Response) => {
     console.error(
       colors.red("An error has occurred creating the product"),
       error
+    );
+  }
+};
+
+export const updateProduct = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const product = await Product.findByPk(id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    await product.update(req.body); // -> Update the register but doesn't in the DB.
+    await product.save(); // -> Save the updated product in the DB.
+
+    res.status(200).json({message: "Product updated succesfully", product });
+
+  } catch (error) {
+    console.error(
+      colors.red("An error has occurred white updating the product.")
     );
   }
 };
--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,12 +1,30 @@
 import { Router } from "express";
-import { body } from 'express-validator';
-import { getProducts, createProduct } from "../controllers/product.controller";
+import { body, param } from "express-validator";
+import {
+  getProducts,
+  getProductsById,
+  createProduct,
+  updateProduct,
+} from "../controllers/product.controller";
+import { handleInputErrors } from "../middlewares";
 
 const router = Router();
 
-router.get('/', getProducts);
+router.get("/", getProducts);
 
-router.post("/",
+router.get(
+  "/:id",
+  param("id")
+    .isInt()
+    .withMessage("Id is invalid")
+    .custom((id) => id >= 0)
+    .withMessage("Id must be positive"),
+  handleInputErrors,
+  getProductsById
+);
+
+router.post(
+  "/",
   // Validation
   // Note: body() hace exactamente lo mismo que check(), solo que se
   // utiliza cuando no se puede usar el async await.
@@ -20,6 +38,21 @@ router.post("/",
     .withMessage("Price cannot be empty"),
 
   createProduct
+);
+
+router.put(
+  "/:id",
+  body("name").notEmpty().withMessage("Name cannot be empty"),
+  body("price")
+    .isNumeric()
+    .withMessage("Price must be a number")
+    .custom((value) => value >= 0)
+    .withMessage("Price cannot be negative")
+    .notEmpty()
+    .withMessage("Price cannot be empty"),
+  body("availabilty").isBoolean().withMessage("Availability is invalid"),
+  handleInputErrors,
+  updateProduct
 );
 
 export default router;
